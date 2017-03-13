523a4e901bfccfead5636f6fe151ccefed7ea5d0
jackyjiao <jackyjiao@tencent.com>
2017-03-13 21:55:20 +0800

    init

---
+++ JKSayHi/JKSayHi/JKSayHi.h
@@ -0,0 +1,15 @@
+//
+//  JKSayHi.h
+//  JKSayHi
+//
+//  Created by jackyjiao on 3/13/17.
+//  Copyright © 2017 jackyjiao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JKSayHi : NSObject
+
+- (void)speek;
+
+@end
---
+++ JKSayHi/JKSayHi/JKSayHi.m
@@ -0,0 +1,17 @@
+//
+//  JKSayHi.m
+//  JKSayHi
+//
+//  Created by jackyjiao on 3/13/17.
+//  Copyright © 2017 jackyjiao. All rights reserved.
+//
+
+#import "JKSayHi.h"
+
+@implementation JKSayHi
+
+- (void)speek {
+    NSLog(@"JK is Say Hi to You!");
+}
+
+@end
